9e96878ac4e08b88eb7c8b0f77e6c3568457e96d
Afrisham11 <ds.afrisham@gmail.com>
2026-07-19 06:55:36 -0700

    Add files via upload
    SQL practice demonstrating the use of the WHERE clause to filter records based on specific conditions using comparison and logical operators.

---
+++ where clause in sql.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM employeedb.employee_demographics; 
+
+USE employeedb;
+SELECT *
+FROM employeedb.employee_salary
+WHERE salary > 50000;
+
+select*
+FROM employeedb.employee_salary
+WHERE salary  <= 50000;
+
+
+select * 
+from employeedb.employee_demographics
+where first_name = "april"
+;
+
+select * 
+from employeedb.employee_demographics
+where gender = "male"
+;
+
+
+
+
