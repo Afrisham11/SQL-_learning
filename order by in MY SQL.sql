f4120eebf90e9dda40b3669f21689b5d8251e716
Afrisham11 <ds.afrisham@gmail.com>
2026-07-20 10:16:06 -0700

    Add files via upload
    SQL practice demonstrating the use of the ORDER BY clause to sort query results in ascending and descending order using single and multiple columns.

---
+++ order by in MY SQL.sql
@@ -0,0 +1,47 @@
+# - - order by 
+select * 
+from employeedb.employee_salary
+;
+
+select * 
+from employeedb.employee_salary
+order by salary
+;
+
+select * 
+from employeedb.employee_salary
+order by salary desc
+;
+
+select * 
+from employeedb.employee_salary
+order by first_name
+;
+
+select * 
+from employeedb.employee_salary
+order by last_name
+;
+select * 
+from employeedb.employee_salary;
+
+select* 
+from employeedb.employee_demographics
+order by first_name 
+;
+
+select * 
+from employeedb.employee_salary
+order by salary desc ,first_name asc
+;
+
+select * 
+from employeedb.parks_departments
+;
+
+SELECT *
+FROM employeedb.parks_departments
+ORDER BY department_name ASC
+;
+
+
